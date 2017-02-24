1ea8271c70b92a09309f0216a759a4d8db2264a4
magaiverf <magaiverf@gmail.com>
2017-02-23 22:14:36 -0300

    Initial Commit

---
+++ src/app/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+using ClassLibrary;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+            WriteLine($"The answer is {new Thing().Get(19, 23)}");
+        }
+    }
+}
---
+++ test/test-library/Tests.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibrary;
+using Xunit;
+
+namespace TestApp
+{
+    public class LibraryTests
+    {
+        [Fact]
+        public void TestThing() {
+            Assert.Equal(42, new Thing().Get(19, 23));
+        }
+    }
+}
